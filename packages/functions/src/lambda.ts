29af8bc9729ea8bbc3c42ea2aa446a32fd78d940
knightsarai <knight.sarai.dev@gmail.com>
2024-05-17 23:15:16 +0300

    Adding a storage stack

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from "sst/node/api";
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hello knight. The time is ${new Date().toISOString()}`,
-  };
-});
