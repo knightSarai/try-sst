4bd6b5e2f13f8698f0f52d24607f6c0df0d783ac
knightsarai <knight.sarai.dev@gmail.com>
2024-05-18 00:52:57 +0300

    Add create note enpoint

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,30 @@
+import * as uuid from "uuid";
+import { Table } from "sst/node/table";
+import dynamoDb from "@try-sst/core/dynamodb";
+import handler from "@try-sst/core/handler";
+
+export const main = handler(async (event) => {
+  let data = {
+    content: "",
+    attachment: "",
+  };
+
+  if (event.body != null) {
+    data = JSON.parse(event.body);
+  }
+
+  const params = {
+    TableName: Table.Notes.tableName,
+    Item: {
+      userId: "123",
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return JSON.stringify(params.Item);
+});
