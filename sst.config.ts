6b6d942c9b0db78f45b772e64cbfb29e8c791e80
knightsarai <knight.sarai.dev@gmail.com>
2024-05-18 15:16:35 +0300

    Init frontend app

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 
 export default {
@@ -15,6 +16,7 @@ export default {
     app
       .stack(StorageStack)
       .stack(ApiStack)
-      .stack(AuthStack);
+      .stack(AuthStack)
+      .stack(FrontendStack);
   },
 } satisfies SSTConfig;
